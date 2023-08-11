3156001a97d5c611fababd6546767006ca102eea
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-08-11 20:16:10 +0800

    Remove Vale linter

--- lua/modules/tools/efmls.lua
+++ lua/modules/tools/efmls.lua
@@ -4,12 +4,10 @@ if not setup then
   return
 end
 
-local vale = require("efmls-configs.linters.vale")
 local shellcheck = require("efmls-configs.linters.shellcheck")
 
 efmls.init({ default_config = false })
 
 efmls.setup({
-  markdown = { linter = vale },
   sh = { linter = shellcheck }
 })
