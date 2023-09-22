3ca5859e5a3fe12d5c0606d2d4e93bc7a856d4be
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-09-22 16:15:48 +0800

    Remove `opts` setup for nvim-autopairs

--- lua/modules/editor/autopairs.lua
+++ lua/modules/editor/autopairs.lua
@@ -1,19 +1,5 @@
 return {
   "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  opts = {
-    check_ts = true,
-    fast_wrap = {
-      map = "<M-e>",
-      chars = { "{", "[", "(", '"', "'" },
-      pattern = [=[[%'%"%>%]%)%}%,]]=],
-      offset = 0,
-      end_key = "$",
-      keys = "qwertyuiopzxcvbnmasdfghjkl",
-      check_comma = true,
-      manual_position = true,
-      highlight = "Search",
-      highlight_grey = "Comment"
-    }
-  }
+  event = "InsertEnter",
+  config = true
 }
