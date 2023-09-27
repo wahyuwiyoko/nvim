26f9f69e450a6f87cfd32702d331e823e9967bc5
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-09-27 11:11:07 +0800

    Add cmp-nvim-lsp-signature-help plugin for completion

--- lua/modules/editor/cmp.lua
+++ lua/modules/editor/cmp.lua
@@ -5,6 +5,7 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
     "saadparwaiz1/cmp_luasnip",
     {
       "L3MON4D3/LuaSnip",
@@ -58,6 +59,7 @@ return {
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
         { name = "luasnip" },
         { name = "buffer" },
         { name = "path" }
