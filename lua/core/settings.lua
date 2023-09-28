0afd696a8e3d412591741db0959daefbdf527b71
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-09-28 12:10:39 +0800

    Disable some built-in plugins in `settings.lua`

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -2,15 +2,22 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Disable netrw
+-- Disable some built-in plugins
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.editorconfig = false
+vim.g.loaded_gzip = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_spellfile_plugin = 1
 
--- Providers
+-- Disable "expandtab" for markdown
+vim.g.markdown_recommended_style = 0
+
+-- Disable providers
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
-
--- Fix markdown indentation
-vim.g.markdown_recommended_style = 0
