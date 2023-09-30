c600174397d41e7d8452bb015775411a1f35be86
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-09-30 15:04:31 +0800

    Update Treesitter config
    Remove the `config()` and change to `main` for specify the main
    module to use the `opts()`.
    
    Plugin specs: https://github.com/folke/lazy.nvim#-plugin-spec

--- lua/modules/editor/treesitter.lua
+++ lua/modules/editor/treesitter.lua
@@ -2,21 +2,19 @@ return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
-  cmd = "TSUpdateSync",
-  config = function ()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c", "lua", "luadoc", "vim", "vimdoc", "query", "regex",
-        "bash", "json", "yaml",
-        "html", "css", "javascript",
-        "go", "gomod",
-        "markdown", "markdown_inline",
-        "gitcommit", "gitignore", "git_config"
-      },
-      sync_install = false,
-      auto_install = false,
-      highlight = { enable = true },
-      indent = { enable = true }
-    })
-  end
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "c", "lua", "luadoc", "vim", "vimdoc", "query", "regex",
+      "bash", "json", "yaml",
+      "html", "css", "javascript",
+      "go", "gomod",
+      "markdown", "markdown_inline",
+      "gitcommit", "gitignore", "git_config"
+    },
+    sync_install = false,
+    auto_install = false,
+    highlight = { enable = true },
+    indent = { enable = true }
+  },
 }
