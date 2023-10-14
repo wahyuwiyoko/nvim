631242defeeacb2a31d40ee703c18a6cf3f535f6
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-10-14 13:58:01 +0800

    Move LuaSnip loaders to LuaSnip config

--- lua/modules/editor/completion.lua
+++ lua/modules/editor/completion.lua
@@ -9,13 +9,11 @@ return {
     "saadparwaiz1/cmp_luasnip",
     {
       "L3MON4D3/LuaSnip",
-      dependencies = {
-        "rafamadriz/friendly-snippets",
-        config = function ()
-          require("luasnip.loaders.from_vscode").lazy_load()
-        end
-      }
-    },
+      dependencies = { "rafamadriz/friendly-snippets" },
+      config = function ()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end
+    }
   },
   config = function ()
     local cmp = require("cmp")
