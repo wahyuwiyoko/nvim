221c1b2e7133bbdfb5ea9da227a73947f772479a
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-10-20 15:23:46 +0800

    Add `cmp-nvim-lua` plugin and reorder the `cmp` dependencies

--- lua/modules/editor/completion.lua
+++ lua/modules/editor/completion.lua
@@ -2,11 +2,12 @@ return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-nvim-lua",
     "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
     {
       "L3MON4D3/LuaSnip",
       version = "2.x.x",
