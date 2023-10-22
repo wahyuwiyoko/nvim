bd32a26764ad91be9c3de3320ac3c2b7def89e78
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-10-22 10:02:18 +0800

    Update color scheme configuration
    - Change Rose Pine version to `1.x.x`
    - Hide end of buffer (tilde character) color with base color

--- lua/modules/editor/colorscheme.lua
+++ lua/modules/editor/colorscheme.lua
@@ -1,13 +1,16 @@
 return {
   "rose-pine/neovim",
-  version = "1.x",
+  version = "1.x.x",
   lazy = false,
   priority = 1000,
   name = "rose-pine",
   main = "rose-pine",
   opts = {
     variant = "main",
-    disable_italics = true
+    disable_italics = true,
+    highlight_groups = {
+      EndOfBuffer = { fg = "base" }
+    }
   },
   init = function ()
     vim.cmd("colorscheme rose-pine")
