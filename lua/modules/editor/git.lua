bb80dfc359edefc675d8e6b0aa7215f4105bf897
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-10-24 12:06:08 +0800

    Add `stage_hunk` & `stage_buffer` for Gitsigns keymaps

--- lua/modules/editor/git.lua
+++ lua/modules/editor/git.lua
@@ -6,7 +6,9 @@ return {
     { "<Leader>gn", "<Cmd>Gitsigns next_hunk<CR>", desc = "Git next hunk" },
     { "<Leader>gp", "<Cmd>Gitsigns prev_hunk<CR>", desc = "Git previous hunk" },
     { "<Leader>gh", "<Cmd>Gitsigns preview_hunk<CR>", desc = "Git preview hunk" },
-    { "<Leader>gs", "<Cmd>Gitsigns diffthis<CR>", desc = "Git show diff" }
+    { "<Leader>gsh", "<Cmd>Gitsigns stage_hunk<CR>", desc = "Git stage hunk" },
+    { "<Leader>gsb", "<Cmd>Gitsigns stage_buffer<CR>", desc = "Git stage buffer" },
+    { "<Leader>gdf", "<Cmd>Gitsigns diffthis<CR>", desc = "Git show diff" }
   },
   opts = {
     signs = {
