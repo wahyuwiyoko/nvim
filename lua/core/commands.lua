6037fc5ed17fef80bdbbe37a673a1bb1d25d53ee
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-10-25 15:39:12 +0800

    Update `commands.lua`
    - Add `AutoCreateDirectory` and `NonUTF8File` auto command
    - Add `JSONFormat` user command
    
    Reference: https://github.com/jdhao/nvim-config

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,23 +1,50 @@
-local function augroup(group_name)
-  return vim.api.nvim_create_augroup(group_name, { clear = true })
+local auto = vim.api.nvim_create_autocmd
+
+local function group(name)
+  vim.api.nvim_create_augroup(name, { clear = true })
 end
 
-local auto = vim.api.nvim_create_autocmd
-local user = vim.api.nvim_create_user_command
+local function user(name, callback)
+  vim.api.nvim_create_user_command(name, callback, { force = true })
+end
 
 auto("BufWritePre", {
-  group = augroup("RemoveTrailingWhitespace"),
+  group = group("RemoveTrailingWhitespace"),
   pattern = "*",
   callback = function ()
     local save_cursor = vim.fn.winsaveview()
+
     vim.cmd([[%s/\s\+$//e]])
+
     vim.fn.winrestview(save_cursor)
   end
 })
 
 auto("TermOpen", {
-  group = augroup("StartTerminalInInsertMode"),
+  group = group("StartTerminalInInsertMode"),
   command = "startinsert | set winfixheight"
+})
+
+auto("BufWritePre", {
+  group = group("AutoCreateDirectory"),
+  pattern = "*",
+  callback = function (context)
+    local directory = vim.fn.fnamemodify(context.file, ":p:h")
+
+    if vim.fn.isdirectory(directory) == 0 then
+      vim.fn.mkdir(directory, "p")
+    end
+  end
+})
+
+auto("BufRead", {
+  group = group("NonUTF8File"),
+  pattern = "*",
+  callback = function ()
+    if vim.bo.fileencoding ~= "utf-8" then
+      vim.notify("File is not in UTF-8 format!", vim.log.levels.WARN)
+    end
+  end
 })
 
 user("MasonInstallLinters", function ()
@@ -29,4 +56,8 @@ user("MasonInstallLinters", function ()
       vim.cmd("MasonInstall " .. linter)
     end
   end
-end, { force = true })
+end)
+
+user("JSONFormat", function ()
+  vim.cmd("%!jq .")
+end)
