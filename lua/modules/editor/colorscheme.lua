d871d6043971ea9f06ac40d305a39ab30f9d2be8
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-10-31 16:46:55 +0800

    Add Rose Pine highlight groups for diff files

--- lua/modules/editor/colorscheme.lua
+++ lua/modules/editor/colorscheme.lua
@@ -8,11 +8,19 @@ return {
       variant = "main",
       disable_italics = true,
       highlight_groups = {
+        -- Editor
         EndOfBuffer = { fg = "base" },
         StatusLine = { fg = "text", bg = "surface" },
         TabLine = { fg = "subtle", bg = "base" },
+
+        -- Telescope
         TelescopeBorder = { fg = "subtle", bg = "surface" },
-        TelescopeTitle = { fg = "text", bold = true }
+        TelescopeTitle = { fg = "text", bold = true },
+
+        -- Diff files
+        ["@attribute.diff"] = { fg = "gold" },
+        ["@text.diff.add"] = { bg = "foam", blend = 20 },
+        ["@text.diff.delete"] = { bg = "love", blend = 20 }
       }
     })
 
