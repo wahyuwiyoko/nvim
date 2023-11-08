167f2b59cf309ef5462149df53cd7722851fa49b
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-11-08 16:38:32 +0800

    Add variable and array snippets for Go

--- snippets/go.lua
+++ snippets/go.lua
@@ -10,5 +10,14 @@ func main() {{
 ]]
 
 return {
-  s("main", fmt(main_func, { i(1) }))
+  s("main", fmt(main_func, { i(1) })),
+  s("v", fmt("{} := {}", { i(1, "name"), i(2, "value") })),
+  s("arr", fmt("var {}", {
+    c(1, {
+      fmt("{} [{}]{}", { i(1, "name"), i(2, "1"), i(3, "type") }),
+      fmt("{} = [{}]{} {{{}}}", {
+        i(1, "name"), i(2, "1"), i(3, "type"), i(4)
+      })
+    })
+  }))
 }
