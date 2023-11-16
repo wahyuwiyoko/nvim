f476112394848d5ec1db4ef0854bcecbed181afd
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-11-16 14:54:16 +0800

    Remove `main_func` snippet for Go

--- snippets/go.lua
+++ snippets/go.lua
@@ -1,16 +1,7 @@
 --- @diagnostic disable: undefined-global
 --# selene: allow(undefined_variable)
 
-local main_func = [[
-package main
-
-func main() {{
-  {}
-}}
-]]
-
 return {
-  s("main", fmt(main_func, { i(1) })),
   s("v", fmt("{} := {}", { i(1, "name"), i(2, "value") })),
   s("arr", fmt("var {}", {
     c(1, {
