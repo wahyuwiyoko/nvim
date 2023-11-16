d8d8205ed8d284b15a64ad233248ca6e032a2591
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-11-16 15:50:47 +0800

    Add snippets for `all` kind of file type

---
+++ snippets/all.lua
@@ -0,0 +1,14 @@
+--- @diagnostic disable: undefined-global
+--# selene: allow(undefined_variable)
+
+return {
+  s("dateBdY", t(os.date("%B %d, %Y"))),
+  s("todo", fmt("{}: {}", {
+    c(1, {
+      i(1, "TODO"), i(2, "FIX"), i(3, "NOTE"),
+      i(4, "WARNING"), i(5, "HACK"), i(6, "PERF"),
+      i(7, "TEST")
+    }),
+    i(2)
+  }))
+}
