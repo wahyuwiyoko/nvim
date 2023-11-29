eb88f739637215b01fc0c5f77d871be131e30dae
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-11-29 09:51:37 +0800

    Add Hugo schema for `yamlls`

--- lua/languages/yamlls.lua
+++ lua/languages/yamlls.lua
@@ -2,7 +2,8 @@
 
 local schemas = {
   ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
-  ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}"
+  ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+  ["https://json.schemastore.org/hugo.json"] = "hugo.yaml"
 }
 
 return {
