fad4863cc49f4c5cd548b204b8c8d3ad4ebdf6a9
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-12-07 12:12:40 +0800

    Remove `cmp-nvim-lua`, `cmp-path`, and `cmp-cmdline` plugin

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -1,12 +1,9 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = { "InsertEnter", "CmdLineEnter" },
+  event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
     "saadparwaiz1/cmp_luasnip"
   },
   config = function ()
@@ -66,38 +63,19 @@ return {
       },
       sources = {
         { name = "nvim_lsp" },
-        { name = "nvim_lua" },
         { name = "luasnip", option = { show_autosnippets = true } },
-        { name = "buffer" },
-        { name = "path" }
+        { name = "buffer" }
       },
       formatting = {
         format = function (entry, vim_item)
           vim_item.menu = ({
             nvim_lsp = "[LSP]",
-            nvim_lua = "[Lua]",
-            luasnip = "[LuaSnip]",
-            buffer = "[Buffer]",
-            path = "[Path]"
+            luasnip = "[Snippet]",
+            buffer = "[Buffer]"
           })[entry.source.name]
 
           return vim_item
         end
-      }
-    })
-
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" }
-      }
-    })
-
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "cmdline" },
-        { name = "path" }
       }
     })
 
