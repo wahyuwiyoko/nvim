ec57e578fdb4143e82d712c549d35175d66bc844
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-12-16 13:53:57 +0800

    Add a few markdown snippets

--- snippets/markdown.lua
+++ snippets/markdown.lua
@@ -1,12 +1,24 @@
 --- @diagnostic disable: undefined-global
 --# selene: allow(undefined_variable)
 
+local code_block = [[
+```{}
+{}
+```
+]]
+
 local alert = [[
 > [!{}]
 > {}
 ]]
 
 return {
+  s("bold", fmt("**{}**", { i(1, "bold") })),
+  s("italic", fmt("*{}*", { i(1, "italic") })),
+  s("strikethrough", fmt("~~{}~~", { i(1, "strikethrough") })),
+  s("link", fmt("[{}]({})", { i(1, "text"), i(2, "link") })),
+  s("img", fmt("![{}]({})", { i(1, "alt"), i(2, "path") })),
+  s("code", fmt(code_block, { i(1, "language"), i(2) })),
   s("alert", fmt(alert, {
     c(1, {
       i(1, "NOTE"), i(2, "TIP"), i(3, "IMPORTANT"),
