ad97fc577736638aa796ddadb11a0dbbb2ad8280
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-01-29 21:20:59 +0800

    Update `merge_table()` in utilities function

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -18,8 +18,12 @@ M.has = function (feat)
   return false
 end
 
-M.merge_table = function (...)
-  return vim.tbl_deep_extend("force", ...)
+M.merge_table = function (table1, table2)
+  if table1 == table2 == nil then return {} end
+  if table1 == nil then return table2 end
+  if table2 == nil then return table1 end
+
+  return vim.tbl_deep_extend("force", table1, table2)
 end
 
 M.map = function (mode, key, commands, opts)
