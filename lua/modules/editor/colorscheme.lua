80d6a8038f65e6f32ed215b4c6aae74ef1a1501a
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-02-04 09:19:31 +0800

    Change color scheme to Dark Moon

--- lua/modules/editor/colorscheme.lua
+++ lua/modules/editor/colorscheme.lua
@@ -1,23 +1,14 @@
 return {
-  "rose-pine/neovim",
+  "wahyuwiyoko/dark-moon.nvim",
   lazy = false,
   priority = 1000,
-  name = "rose-pine",
   config = function()
-    require("rose-pine").setup({
-      variant = "main",
+    require("dark-moon").setup({
       styles = {
         bold = false,
-        italic = false,
-      },
-      highlight_groups = {
-        StatusLine = { fg = "text", bg = "surface" },
-        TabLine = { fg = "subtle", bg = "base" },
-        TelescopeTitle = { fg = "text", bold = true },
-        TelescopeBorder = { fg = "subtle", bg = "surface" },
       },
     })
 
-    vim.cmd.colorscheme("rose-pine")
+    vim.cmd.colorscheme("dark-moon")
   end,
 }
