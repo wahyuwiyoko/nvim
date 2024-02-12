b0c878d586bbc4865eeff709455cef2562c109da
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-02-12 22:34:27 +0800

    Remove some Vim option that already set the default values

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -5,35 +5,29 @@ local indent_size = 4
 opt.clipboard = "unnamedplus"
 opt.mouse = ""
 opt.wrap = false
-opt.wildmenu = true
 opt.scrolloff = 4
 opt.sidescrolloff = 4
-opt.belloff = "all"
 opt.grepprg = "rg --vimgrep"
-opt.backspace = { "indent", "eol", "start" }
-opt.nrformats:append("alpha")
+opt.confirm = true
+opt.timeoutlen = 300
+opt.completeopt = { "menu", "menuone", "noselect" }
 
 -- File
-opt.encoding = "utf-8"
-opt.fileencoding = "utf-8"
 opt.fileformat = "unix"
 opt.undofile = true
-opt.backup = false
 opt.writebackup = false
 opt.swapfile = false
 
 -- Appearance
 opt.termguicolors = true
-opt.background = "dark"
-opt.showtabline = 1
-opt.signcolumn = "auto"
+opt.number = true
+opt.relativenumber = true
 opt.colorcolumn = "80"
 opt.shortmess:append("sI")
 opt.pumheight = 10
 opt.laststatus = 3
 opt.showcmd = false
 opt.showmode = false
-opt.cursorline = false
 opt.ruler = false
 opt.emoji = false
 opt.fillchars = {
@@ -42,32 +36,16 @@ opt.fillchars = {
   foldclose = ">",
 }
 
--- Line numbers
-opt.number = true
-opt.relativenumber = true
-opt.numberwidth = 4
-
--- Buffer
-opt.confirm = true
-opt.conceallevel = 0
-opt.timeoutlen = 300
-opt.updatetime = 300
-opt.completeopt = { "menu", "menuone", "noselect" }
-
 -- Indentation
 opt.tabstop = indent_size
 opt.softtabstop = indent_size
 opt.shiftwidth = indent_size
 opt.expandtab = true
-opt.smarttab = true
-opt.autoindent = true
 opt.smartindent = true
 
 -- Search
 opt.ignorecase = true
 opt.smartcase = true
-opt.hlsearch = true
-opt.incsearch = true
 
 -- Split window
 opt.splitright = true
