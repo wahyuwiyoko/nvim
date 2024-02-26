4ff7ea58e700e35967dcb55e9021905d1816a9cb
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-02-26 21:54:19 +0800

    Remove schemas description for JSON LSP

--- lua/lsp/servers/jsonls.lua
+++ lua/lsp/servers/jsonls.lua
@@ -3,17 +3,14 @@ local config = require("lsp.config")
 -- Find more: https://www.schemastore.org/json
 local schemas = {
   {
-    description = "TypeScript compiler configuration file",
     fileMatch = { "tsconfig.json", "tsconfig.*.json" },
     url = "https://json.schemastore.org/tsconfig.json",
   },
   {
-    description = "NPM configuration file",
     fileMatch = { "package.json" },
     url = "https://json.schemastore.org/package.json",
   },
   {
-    description = "OpenAPI specification file",
     fileMatch = { "*api*.json" },
     url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json",
   },
