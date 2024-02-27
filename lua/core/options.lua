dac4b3a4b91487ecab9b91941fdb467b1adadf46
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-02-27 17:05:53 +0800

    Update Vim options
    - Ignore case for `grep`
    - Override `path` to value of current directory and recursive
    - Disable cursor GUI

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,41 +4,21 @@ local indent_size = 4
 -- Editor
 opt.clipboard = "unnamedplus"
 opt.mouse = ""
-opt.wrap = false
-opt.scrolloff = 4
-opt.sidescrolloff = 4
-opt.grepprg = "rg --vimgrep"
 opt.confirm = true
 opt.timeoutlen = 300
+opt.grepprg = "rg --vimgrep --ignore-case"
 opt.completeopt = { "menu", "menuone", "noselect" }
+opt.path = { ".", "**", "" }
+opt.wildmode = { "longest:full", "full" }
+opt.wildoptions = { "fuzzy", "pum" }
+opt.wildignore = { "**/node_modules/**", "**/vendor/**", "**/.git/**" }
+opt.wildignorecase = true
 
 -- File
 opt.fileformat = "unix"
 opt.undofile = true
 opt.writebackup = false
 opt.swapfile = false
-opt.path = { ".", "", "**" }
-opt.wildmode = { "longest:full", "full" }
-opt.wildignore = { "**/node_modules/**", "**/vendor/**", "**/.git/**" }
-opt.wildignorecase = true
-
--- Appearance
-opt.termguicolors = true
-opt.number = true
-opt.relativenumber = true
-opt.colorcolumn = "80"
-opt.shortmess:append("sI")
-opt.pumheight = 10
-opt.laststatus = 3
-opt.showcmd = false
-opt.showmode = false
-opt.ruler = false
-opt.emoji = false
-opt.fillchars = {
-  eob = " ",
-  foldopen = "v",
-  foldclose = ">",
-}
 
 -- Indentation
 opt.tabstop = indent_size
@@ -47,10 +27,32 @@ opt.shiftwidth = indent_size
 opt.expandtab = true
 opt.smartindent = true
 
+-- Window
+opt.scrolloff = 4
+opt.sidescrolloff = 4
+opt.splitright = true
+opt.splitbelow = true
+
 -- Search
 opt.ignorecase = true
 opt.smartcase = true
 
--- Split window
-opt.splitright = true
-opt.splitbelow = true
+-- UI
+opt.termguicolors = true
+opt.guicursor = ""
+opt.wrap = false
+opt.number = true
+opt.relativenumber = true
+opt.colorcolumn = "80"
+opt.pumheight = 10
+opt.laststatus = 3
+opt.showcmd = false
+opt.showmode = false
+opt.ruler = false
+opt.emoji = false
+opt.shortmess:append("sI")
+opt.fillchars = {
+  eob = " ",
+  foldopen = "v",
+  foldclose = ">",
+}
