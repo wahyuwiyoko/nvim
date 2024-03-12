84effd348280172798095381efe0014262fddfb9
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-03-12 16:25:41 +0800

    Add bang for check if nvim is not installed

--- setup/requirement.sh
+++ setup/requirement.sh
@@ -33,7 +33,7 @@ if ! is_dir_exists "$HOME/software"; then
   mkdir -p "$HOME/software"
 fi
 
-if has_executable nvim; then
+if ! has_executable nvim; then
   wget -c --show-progress https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
   tar xzf nvim-linux64.tar.gz
   rm nvim-linux64.tar.gz
